62dc6a7ef9d3b020da0e71298973a47ad0d54044
gk8270 <gk8270@srmist.edu.in>
2026-03-10 10:23:43 +0530

    UC10:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,19 +2,30 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "deed";
+        String phrase = "A man a plan a canal Panama";
 
-        if (isPalindrome(word, 0, word.length() - 1)) {
-            System.out.println(word + " is a Palindrome");
+        String normalized = phrase.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(normalized)) {
+            System.out.println("\"" + phrase + "\" is a Palindrome");
         } else {
-            System.out.println(word + " is not a Palindrome");
+            System.out.println("\"" + phrase + "\" is not a Palindrome");
         }
 
     }
 
-    static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) return true;
-        if (str.charAt(start) != str.charAt(end)) return false;
-        return isPalindrome(str, start + 1, end - 1);
+    static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
