11b9e19aa203d64f423159ba6578cb2c00de8c74
gk8270 <gk8270@srmist.edu.in>
2026-03-10 10:04:00 +0530

    UC4:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,14 +2,23 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "racecar";
-        String reverse = "";
+        String word = "level";
+        char[] chars = word.toCharArray();
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reverse = reverse + word.charAt(i);
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        if (word.equals(reverse)) {
+        if (isPalindrome) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
