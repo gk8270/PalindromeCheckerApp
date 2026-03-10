2f22bb1ef8d6e30243a9d7545905dad363bf085c
gk8270 <gk8270@srmist.edu.in>
2026-03-10 09:59:04 +0530

    UC3:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,7 +2,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "madam";
+        String word = "racecar";
         String reverse = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
