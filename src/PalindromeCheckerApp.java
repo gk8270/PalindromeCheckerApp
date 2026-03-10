17bc9a01c6cfe6a1870050da39a2704dc0d1c437
gk8270 <gk8270@srmist.edu.in>
2026-03-10 09:48:08 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,11 @@
 public class PalindromeCheckerApp {
-   public static void main(String[] args){
-       System.out.println("Hello world");
-   }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to PalindromeCheckerApp");
+        System.out.println("Application Name: Palindrome Checker");
+        System.out.println("Version: 1.0");
+        System.out.println("Program started successfully.");
+
+    }
 }
