f259b022969749fc0068dfd534a0b0c85af96f93
gk8270 <gk8270@srmist.edu.in>
2026-03-10 10:17:09 +0530

    UC9:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,10 @@
 public class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-        Node(char data) { this.data = data; }
-    }
-
     public static void main(String[] args) {
 
-        String word = "madam";
-        Node head = buildLinkedList(word);
+        String word = "deed";
 
-        if (isPalindrome(head)) {
+        if (isPalindrome(word, 0, word.length() - 1)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
@@ -19,62 +12,9 @@ public class PalindromeCheckerApp {
 
     }
 
-    static Node buildLinkedList(String word) {
-        Node head = null;
-        Node tail = null;
-        for (int i = 0; i < word.length(); i++) {
-            Node node = new Node(word.charAt(i));
-            if (head == null) {
-                head = node;
-                tail = node;
-            } else {
-                tail.next = node;
-                tail = node;
-            }
-        }
-        return head;
-    }
-
-    static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle using fast and slow pointers
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node secondHalf = reverse(slow.next);
-
-        // Compare first half and reversed second half
-        Node firstHalf = head;
-        Node tempSecond = secondHalf;
-        boolean isPalin = true;
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                isPalin = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
-        }
-
-        return isPalin;
-    }
-
-    static Node reverse(Node head) {
-        Node prev = null;
-        Node curr = head;
-        while (curr != null) {
-            Node next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+    static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
     }
 }
